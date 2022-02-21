eab572ff6eb0eeb059f20fe39d66a7970f8adb9f
Yuhang Qian <yuhang886688@ucla.com>
2022-02-21 23:58:56 +0000

    hw3 almost finished

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -1,5 +1,6 @@
 # Load packages
 library(shiny)
+library(tidyverse)
 
 # Load data
 icu_cohort <- readRDS("icu_cohort.rds")
@@ -8,34 +9,97 @@ icu_cohort <- readRDS("icu_cohort.rds")
 ui <- fluidPage(
   titlePanel("Summaries of the ICU cohort data"),
   
-  sidebarLayout(
-    sidebarPanel(
-      helpText("lab measurements"),
-      
-      selectInput("labvar", 
-                  label = "lab measurements",
-                  choices = c("calcium", "magnesium",
-                              "potassium", "chloride",
-                              "sodium", "creatinine",
-                              "bicarbonate", "glucose",
-                              "hematocrit", "wbc"),
-                  selected = "calcium"),
-      
+  tabsetPanel(
+    
+    tabPanel("demo",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("demovar", 
+                             label = "demographics",
+                             choices = c("gender", "language",
+                                         "marital_status", "ethnicity",
+                                         "insurance", "thirty_day_mort"),
+                             selected = "gender"),
+                 
+               ),
+               
+               mainPanel(plotOutput("demodistPlot"))
+             )
+             
     ),
     
-    mainPanel(plotOutput("labdistPlot"))
+    
+    tabPanel("lab",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput("labvar", 
+                        label = "lab measurements",
+                        choices = c("calcium", "magnesium",
+                                    "potassium", "chloride",
+                                    "sodium", "creatinine",
+                                    "bicarbonate", "glucose",
+                                    "hematocrit", "wbc"),
+                        selected = "calcium"),
+            
+          ),
+          
+          mainPanel(sliderInput("labbins",
+                      "Number of bins for histogram:",
+                      min = 1,
+                      max = 50,
+                      value = 30),
+                    plotOutput("labdistPlot"))
+        )
+    
+    ),
+    tabPanel("chart",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("vitalvar", 
+                             label = "vitals measurements",
+                             choices = c("meanbp", "sbp",
+                                         "temp", "resp_rate",
+                                         "heartrate"),
+                             selected = "meanbp"),
+                 
+               ),
+               
+               mainPanel(sliderInput("vitalbins",
+                                     "Number of bins for histogram:",
+                                     min = 1,
+                                     max = 50,
+                                     value = 30),
+                         plotOutput("vitaldistPlot"))
+             )
+             
+    )
+    
+    
   )
 )
 
 
+
 # Server logic
 server <- function(input, output) {
-    
-    output$labdistPlot <- renderPlot({
-      x <- icu_cohort[[input$labvar]]
-      hist(x, col = 'darkgray', border = 'white')
+  
+    output$demodistPlot <- renderPlot({
+      ggplot() +
+      geom_bar(mapping = aes(y = icu_cohort[[input$demovar]]), na.rm = TRUE) +
+        labs(y = input$demovar)
     })
-
+  
+    output$labdistPlot <- renderPlot({
+      ggplot(icu_cohort, aes(x = .data[[input$labvar]])) +
+        geom_histogram(bins = input$labbins, na.rm = TRUE) +
+        labs(x = input$labvar)
+    })
+    
+    output$vitaldistPlot <- renderPlot({
+      ggplot(icu_cohort, aes(x = .data[[input$vitalvar]])) +
+        geom_histogram(bins = input$vitalbins, na.rm = TRUE) +
+        labs(x = input$vitalvar)
+    })
 }
     
 # Run the application 
