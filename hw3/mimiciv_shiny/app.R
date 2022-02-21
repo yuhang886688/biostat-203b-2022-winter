f25525f77629aaf9b3d743c346c7a79c109b685b
Yuhang Qian <yuhang886688@ucla.com>
2022-02-21 08:42:36 +0000

    hw3 shiny app continue

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -2,8 +2,41 @@
 library(shiny)
 
 # Load data
-icu_cohort <- readRDS("./mimiciv_shiny/icu_cohort.rds")
+icu_cohort <- readRDS("icu_cohort.rds")
+
+# User interface
+ui <- fluidPage(
+  titlePanel("Summaries of the ICU cohort data"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("lab measurements"),
+      
+      selectInput("labvar", 
+                  label = "lab measurements",
+                  choices = c("calcium", "magnesium",
+                              "potassium", "chloride",
+                              "sodium", "creatinine",
+                              "bicarbonate", "glucose",
+                              "hematocrit", "wbc"),
+                  selected = "calcium"),
+      
+    ),
+    
+    mainPanel(plotOutput("labdistPlot"))
+  )
+)
 
 
+# Server logic
+server <- function(input, output) {
+    
+    output$labdistPlot <- renderPlot({
+      x <- icu_cohort[[input$labvar]]
+      hist(x, col = 'darkgray', border = 'white')
+    })
+
+}
+    
 # Run the application 
 shinyApp(ui = ui, server = server)
